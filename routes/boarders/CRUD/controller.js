aec8448f533c269cd35f811534411a06edd32e8b
chowhr1231 <thddbswo14@gmail.com>
2019-02-04 16:51:15 +0900

    [UPDATE] 20190204 post /contents api 구현

--- routes/boarders/CRUD/controller.js
+++ routes/boarders/CRUD/controller.js
@@ -1,6 +1,6 @@
 const multer = require('multer');
 const path = require('path');
-const {Story, StoryHashtags} = require('../../../models/index');
+const {story, storyHashtags, content} = require('../../../models/index');
 
 
 // imagae 저장시 어디에 저장할지 정하는 코드
@@ -34,30 +34,30 @@ let uploads = exports.upload = upload.single('image');
 exports.createStroy = async (req, res) => {
     // token에서 user_id를 받는다.
     const userId = 3;
-    let story = {};
+    let storyObj = {};
     let hashtags = [];
 
-    story = req.body;
-    story['userId'] = userId;
+    storyObj = req.body;
+    storyObj['user_id'] = userId;
     if(req.file != null)
-        story['image_url'] = req.file.filename;
+        storyObj['image_url'] = req.file.filename;
 
     // result: create작업 후 생성된 객체를 담는 변수
-    let result = await Story.create(story);
+    let result = await story.create(storyObj);
 
     /*
         result.dataValues.id: 생성된 객체에서 작업으로 생성된 id값을 의미
      */
-    for (i in story.hashtagId) {
+    for (i in storyObj.hashtagId) {
         let obj = {}
-        obj.storyId = result.dataValues.id;
-        obj.hashtagId = parseInt(story.hashtagId[i]);
+        obj.story_id = result.dataValues.id;
+        obj.hashtag_id = parseInt(storyObj.hashtagId[i]);
 
         hashtags.push(obj);
     }
 
     for (i in hashtags) {
-        StoryHashtags.create(hashtags[i]);
+        storyHashtags.create(hashtags[i]);
     }
 
     res.status(201);
@@ -66,17 +66,22 @@ exports.createStroy = async (req, res) => {
     });
 };
 
-exports.createContent = (req, res) => {
+exports.createContent = async (req, res) => {
     // token으로 user_id값을 받는다.
     let userId = 3;
-    let storyId = 20;
+    let storyId = 1;
 
     /*
-        req.body에 담긴 데이터:   
+        req.body에 담긴 데이터
+        title, description, image_url
     */
-    let content = {};
-    content = req.body;
-    content['image'] = req.file;
+    let contentObj = {};
+    contentObj = req.body;
+    contentObj['image_url'] = req.file.filename;
+    contentObj['user_id'] = userId;
+    contentObj['story_id'] = storyId;
+
+    await content.create(contentObj);
 
     res.status(201);
     res.json({
