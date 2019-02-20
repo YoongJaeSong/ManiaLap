dc0be5e691c57f6a3fb17b74b79d6eef8f6d7dc4
chowhr1231 <thddbswo14@gmail.com>
2019-02-20 15:40:59 +0900

    [MOD] 모든 id값에 autoIncrement 추가

--- models/story_hashtags.js
+++ models/story_hashtags.js
@@ -1,30 +1,30 @@
 /* jshint indent: 2 */
 
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('story_hashtags', {
-    id: {
-      type: DataTypes.INTEGER(11),
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    story_id: {
-      type: DataTypes.INTEGER(11),
-      allowNull: false,
-      references: {
-        model: 'stories',
-        key: 'id'
-      }
-    },
-    hashtag_id: {
-      type: DataTypes.INTEGER(11),
-      allowNull: false,
-      references: {
-        model: 'hashtags',
-        key: 'id'
-      }
-    }
-  }, {
-    tableName: 'story_hashtags'
-  });
+module.exports = function (sequelize, DataTypes) {
+    return sequelize.define('story_hashtags', {
+        id: {
+            type: DataTypes.INTEGER(11),
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        story_id: {
+            type: DataTypes.INTEGER(11),
+            allowNull: false,
+            references: {
+                model: 'stories',
+                key: 'id'
+            }
+        },
+        hashtag_id: {
+            type: DataTypes.INTEGER(11),
+            allowNull: false,
+            references: {
+                model: 'hashtags',
+                key: 'id'
+            }
+        }
+    }, {
+        tableName: 'story_hashtags'
+    });
 };
--- models/user_follow_designers.js
+++ models/user_follow_designers.js
@@ -1,30 +1,30 @@
 /* jshint indent: 2 */
 
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('user_follow_designers', {
-    id: {
-      type: DataTypes.INTEGER(11),
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    user_id: {
-      type: DataTypes.INTEGER(11),
-      allowNull: false,
-      references: {
-        model: 'users',
-        key: 'id'
-      }
-    },
-    designer_id: {
-      type: DataTypes.INTEGER(11),
-      allowNull: false,
-      references: {
-        model: 'users',
-        key: 'id'
-      }
-    }
-  }, {
-    tableName: 'user_follow_designers'
-  });
+module.exports = function (sequelize, DataTypes) {
+    return sequelize.define('user_follow_designers', {
+        id: {
+            type: DataTypes.INTEGER(11),
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        user_id: {
+            type: DataTypes.INTEGER(11),
+            allowNull: false,
+            references: {
+                model: 'users',
+                key: 'id'
+            }
+        },
+        designers_id: {
+            type: DataTypes.INTEGER(11),
+            allowNull: false,
+            references: {
+                model: 'designers',
+                key: 'id'
+            }
+        }
+    }, {
+        tableName: 'user_follow_designers'
+    });
 };
