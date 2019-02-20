d4bf785d8e5f1fb309f3523d42177d5c22e27bdd
chowhr1231 <thddbswo14@gmail.com>
2019-02-20 15:37:51 +0900

    [ADD] 테이블 변경으로 designers, comments, users_designers_comments파일 추가

---
+++ models/designers.js
@@ -0,0 +1,39 @@
+/* jshint indent: 2 */
+
+module.exports = function (sequelize, DataTypes) {
+    return sequelize.define('designers', {
+        id: {
+            type: DataTypes.INTEGER(11),
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING(45),
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING(200),
+            allowNull: true
+        },
+        register_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+        },
+        register_type: {
+            type: DataTypes.INTEGER(11),
+            allowNull: false
+        },
+        users_id: {
+            type: DataTypes.INTEGER(11),
+            allowNull: false,
+            references: {
+                model: 'users',
+                key: 'id'
+            }
+        }
+    }, {
+        tableName: 'designers'
+    });
+};
